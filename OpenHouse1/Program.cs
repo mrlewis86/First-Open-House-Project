dc9208017b3d59fede9e3f231bf8df646f67c55e
Mark Lewis <mrlewis86@gmail.com>
2018-07-18 19:12:37 -0500

    Add project files.

---
+++ OpenHouse1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenHouse1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
